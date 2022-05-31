135f7d15608f1eca5a98901fe6cbcee101f0fd89
hritik19csu129 <hritik19csu129@ncuindia.edu>
2022-05-31 15:05:23 +0530

    MajorCPGD

---
+++ Assets/Player Controller/CamController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using UnityEngine;
+
+public class CamController : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float smoothTime;
+
+    public Transform playerTransform;
+    public void FixedUpdate()
+    {
+        Vector3 pos = GetComponent<Transform>().position;
+
+        pos.x = Mathf.Lerp(pos.x, playerTransform.position.x,smoothTime);
+        pos.y = Mathf.Lerp(pos.y, playerTransform.position.y,smoothTime);
+       
+
+        GetComponent<Transform>().position = pos;
+    }
+}
